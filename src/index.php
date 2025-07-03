4dd06a5935a7ed70cb5f0e9aa30b063616391a62
Hideki Abe <hideki.abe.0630@gmail.com>
2025-07-03 18:55:24 +0900

    記事タイトルの表示を追加

--- src/index.php
+++ src/index.php
@@ -4,3 +4,9 @@ require_once 'class.Prototype.php';
 $app = new Prototype( ['id' => 'Worker'] );
 $app->logging  = true;
 $app->init();
+
+$terms = ['status' => 4];
+$entries = $app->db->model('entry')->load($terms);
+foreach($entries as $entry) {
+    echo $entry->title . PHP_EOL;
+}
